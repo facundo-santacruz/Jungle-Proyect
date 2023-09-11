f2d46f712598d7c86c500947bcb3e6b2d63f6343
Facundo Santacruz <66225293+facundo-santacruz@users.noreply.github.com>
2023-09-11 00:41:16 +0000

    vehicle component style

--- client/src/components/vehicle/Vehicle.jsx
+++ client/src/components/vehicle/Vehicle.jsx
@@ -7,13 +7,13 @@ const vehicle = () => {
   return (
     <div className={styles.primary_container}>
       <div className={styles.secondary_container}>
-        <div className={styles.image_container}>
+        <div className={styles.image_container} onClick={() => console.log("Lancha")}>
+          <p className={styles.text}> Lancha </p>
           <img src={Lancha} className={styles.image} />
-          <text> Lancha </text>
         </div>
-        <div className={styles.secondary_container}>
+        <div className={styles.image_container} onClick={() => { console.log("Camión") }}>
+          <p className={styles.text}>Camión</p>
           <img src={Camion} className={styles.image} />
-          <button> Camion </button>
         </div>
 
       </div>
